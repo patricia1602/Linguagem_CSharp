96d9b79e0a13d553e9f04ae3f7d43b45e151f7d7
patricia1602 <80828415+patricia1602@users.noreply.github.com>
2023-09-02 18:34:42 -0300

    entrada

--- Produtos/Program.cs
+++ Produtos/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace EstoqueLoja {
+
+     class Produto{
+            string Nome{get; set;}
+            int QuantidadeEmEstoque{get; set;}
+
+            Produto(string nome, int quantidadeInicial){
+                Nome = nome;
+                QuantidadeEmEstoque = quantidadeInicial;
+
+            }
+        }
     class Program {
 
      private const string EstoqueLoja = "estoque.txt";  
-
+ 
      static void Main(){
         while(true){
             Console.WriteLine("Menu: ");
@@ -19,22 +31,110 @@ namespace EstoqueLoja {
             Console.WriteLine("Escolha uma opção: ");
 
             switch(Console.ReadLine()){
-                case "1": Novo();
+                case "1":   
+                    Novo();
                 break;
-                case "2": ListarProdutos();
+                case "2": 
+                    ListarProdutos();
                 break;
-                case "3": RemoverProduto();
+                case "3":
+                     RemoverProduto();
                 break;
-                case "0": return;
-                default: Console.WriteLine("Opção inválida!");
+                case "4":
+                     EntradaDeEstoque();
                 break;
-
-            
+                case "5": 
+                   SaidaDeEstoque();
+                break;     
+                case "0":
+                     return;
+                default: 
+                    Console.WriteLine("Opção inválida!");
+                break;
+        
             }
         }        
      }
 
-         static void ListarProdutos(){
+        private static void SaidaDeEstoque(){
+            List<string> produtos = new List<string>();
+
+            if (produtos.Count == 0){
+        Console.WriteLine("Ainda não há produtos adicionados.");
+        return;
+    }
+
+    ListarProdutos();
+
+    Console.Write("Digite o número do produto para saída de estoque: ");
+    
+    if(int.TryParse(Console.ReadLine(), out int numeroProduto)){
+        if(numeroProduto >= 1 && numeroProduto <= produtos.Count){
+            Console.Write("Digite a quantidade da saída: ");
+            
+             if(int.TryParse(Console.ReadLine(), out int quantidadeEntrada)){
+                if (quantidadeEntrada > 0){
+                    string[] produtoInfo = produtos[numeroProduto - 1].Split(':');
+                    string nomeProduto = produtoInfo[0];
+                    int quantidadeAtual = int.Parse(produtoInfo[1]);
+                    quantidadeAtual += quantidadeEntrada;                  
+                
+                    produtos[numeroProduto - 1] = $"{nomeProduto}:{quantidadeAtual}";
+                    File.WriteAllLines(EstoqueLoja, produtos);
+                    Console.WriteLine($"Saida de estoque de {quantidadeEntrada} unidades para {nomeProduto} realizada com sucesso.");
+                }else{
+                    Console.WriteLine("A quantidade de saida deve ser maior que zero.");
+                }
+            }
+           
+        }else{
+           Console.WriteLine("Quantidade de saida inválida.");
+            }
+    }else{
+        Console.WriteLine("Número de produto inválido.");
+        }    
+ }
+
+        private static void EntradaDeEstoque(){
+             List<string> produtos = new List<string>(File.ReadAllLines(EstoqueLoja));
+    
+    if (produtos.Count == 0){
+        Console.WriteLine("Ainda não há produtos adicionados.");
+        return;
+    }
+
+    ListarProdutos();
+
+    Console.Write("Digite o número do produto para entrada de estoque: ");
+    
+    if(int.TryParse(Console.ReadLine(), out int numeroProduto)){
+        if(numeroProduto >= 1 && numeroProduto <= produtos.Count){
+            Console.Write("Digite a quantidade de entrada: ");
+            
+             if(int.TryParse(Console.ReadLine(), out int quantidadeEntrada)){
+                if (quantidadeEntrada > 0){
+                    string[] produtoInfo = produtos[numeroProduto - 1].Split(':');
+                    string nomeProduto = produtoInfo[0];
+                    int quantidadeAtual = int.Parse(produtoInfo[1]);
+                    quantidadeAtual += quantidadeEntrada;                  
+                
+                    produtos[numeroProduto - 1] = $"{nomeProduto}:{quantidadeAtual}";
+                    File.WriteAllLines(EstoqueLoja, produtos);
+                    Console.WriteLine($"Entrada de estoque de {quantidadeEntrada} unidades para {nomeProduto} realizada com sucesso.");
+                }else{
+                    Console.WriteLine("A quantidade de entrada deve ser maior que zero.");
+                }
+            }
+           
+        }else{
+           Console.WriteLine("Quantidade de entrada inválida.");
+            }
+    }else{
+        Console.WriteLine("Número de produto inválido.");
+        }
+    }
+
+        static void ListarProdutos(){
             if (!File.Exists(EstoqueLoja)){
                 Console.WriteLine("Ainda não há produtos adicionados.");
                 return;
